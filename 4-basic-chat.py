8bfa407719445ffa9248dd4a8ead9f3ed69eb385
Jeetendra Singh <chauhanjeetendra@gmail.com>
2025-06-08 01:36:43 +0530

    refactored code

--- 4-basic-chat.py
+++ 4-basic-chat.py
@@ -2,8 +2,8 @@ from pydantic import BaseModel
 from langgraph.graph.message import add_messages
 from typing import List, Annotated, TypedDict, Literal
 from langgraph.graph import START, END, StateGraph
-useLocalLLM = True
 
+useLocalLLM = True
 if useLocalLLM:
     from llms.llama_local import local_llama
     llm = local_llama
@@ -16,12 +16,9 @@ class AgentState(TypedDict):
     next_state: Literal["chat", "END"]
     
 def chat_node(state: AgentState) -> AgentState:
-    print("B4",state["messages"])
     response = llm.invoke(state["messages"])
-
     print(f"Assistant: {response.content}")
 
-    # loop
     user_input = input("You: ")
     if user_input.lower() in ["bye", "exit"]:
         return {
@@ -34,27 +31,20 @@ def chat_node(state: AgentState) -> AgentState:
         "next_state": "chat"
     }
 
-def chat_condition(state: AgentState) -> str:
-    if state["next_state"] == "END":
-        return "END"
-    return "chat"
+
 graph = StateGraph(AgentState)
 
 graph.add_node("chat", chat_node)
 graph.add_edge(START, "chat")
-graph.add_conditional_edges("chat", chat_condition, {
+graph.add_conditional_edges("chat", lambda state: state["next_state"], {
     "chat": "chat",
     "END": END
 })
 
-
 app = graph.compile()
-
 user_input = input("You: ")
-
 initial_state = {
     "messages": [{"role": "user", "content": user_input}],
     "next_step": "chat"
 }
-
 final_state = app.invoke(initial_state)
