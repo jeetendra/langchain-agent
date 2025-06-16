efe9946132a0cfa9687e7a4ed11de3ea116253dd
Jeetendra Singh <chauhanjeetendra@gmail.com>
2025-06-16 23:03:57 +0530

    call openai api for user query

--- agent/client.py
+++ agent/client.py
@@ -1,3 +1,13 @@
+from langchain.chat_models import init_chat_model
+import os
+from dotenv import load_dotenv
+load_dotenv()
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY") or ""
+
+chat_model = init_chat_model(
+    model="gpt-4o-mini",
+)
+
 async def main():
     print("Welcome to the LangGraph Agent Client!")
     print("Type 'quit' or 'exit' to stop the client.")
@@ -7,8 +17,14 @@ async def main():
         if user_input in ["quit", "exit"]:
             break
 
-        print(f"AI: {user_input.capitalize()}")
+        ai_response =get_response(user_input)
 
+        print(f"AI: {ai_response}")
+
+def get_response(user_input):
+    
+    response = chat_model.invoke([{"role": "user", "content": user_input}])
+    return str(response.content)
 
 if __name__ == "__main__":
     import asyncio
